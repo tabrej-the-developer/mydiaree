8645728dcc223913de830b7f5e50def25f9ab76a
Tabrej Khan <tabrezk294@gmail.com>
2025-02-25 14:32:36 +0530

    email changes

--- application/config/email.php
+++ application/config/email.php
@@ -3,11 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 // Email configuration
 $config['protocol']    = 'smtp';
-$config['smtp_host']   = 'smtpout.secureserver.net'; // GoDaddy SMTP server
-$config['smtp_port']   = 465;                        // SSL (or 587 for TLS)
+$config['smtp_host']   = 'smtp.zoho.com';           // Zoho SMTP server
+$config['smtp_port']   = 587;                               // SSL (or 587 for TLS)
 $config['smtp_user']   = 'support@mydiaree.com';     // Your full email address
 $config['smtp_pass']   = 'MyD1@ree';      // Your email password
-$config['smtp_crypto'] = 'ssl';                      // 'tls' for port 587
+$config['smtp_crypto'] = 'tls';                     // 'tls' for port 587
 $config['mailtype']    = 'html';
 $config['charset']     = 'utf-8';
 $config['newline']     = "\r\n";
+$config['wordwrap']    = TRUE;
